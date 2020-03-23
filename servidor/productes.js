c80ddc57fcff632acf1a27deab371b8b9c75226b
albertamaro <57396404+albertamaro@users.noreply.github.com>
2020-03-23 21:59:26 +0100

    Add files via upload

--- servidor/productes.js
+++ servidor/productes.js
@@ -14,43 +14,45 @@ class Obj {
         let sql = '',
             taulaProductesExisteix = false,
             taula = null
-    
+
         // Forçem una espera al fer login amb codi, perquè es vegi la càrrega (TODO: esborrar-ho)
-        await utils.promiseWait(1000) 
-        
+        await utils.promiseWait(1000)
+
         // Mira si la taula "productes" existeix
         try {
             taulaProductesExisteix = await db.promiseTableExists('productes')
         } catch (e) {
             console.warn('Avis, funció login: la taula "productes" no existeix')
         }
-    
+
         // Si la taula "productes" no existeix, en crea una i afegeix productes
         if (!taulaProductesExisteix) {
             try {
                 sql = 'CREATE TABLE productes (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, nom VARCHAR(50) NOT NULL, descripcio TEXT, preu INT(6), imatge VARCHAR(255))'
                 await db.promiseQuery(sql)
-                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Tele", "Tele molt xula", 800, "/web/imatges/producte-1.jpg")'
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Viaje 1", "Viatge a Egipte.", 1847, "/web/imatges/egipte.jpeg")'
                 await db.promiseQuery(sql)
-                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Batidora", "Batidora molt xula", 20, "/web/imatges/producte-2.png")'
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Viatge 2", "Viatge a Japó.", 3230, "/web/imatges/japo.jpeg")'
                 await db.promiseQuery(sql)
-                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Aspirador", "Aspirador molt xulo", 300, "/web/imatges/producte-3.jpg")'
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Viatge 3", "Viatge a Perú.", 2335, "/web/imatges/peru.jpg")'
+                await db.promiseQuery(sql)
+                sql = 'INSERT INTO productes (nom, descripcio, preu, imatge) VALUES ("Viaje 4", "Viatge a Uganda.", 5965, "/web/imatges/uganda.jpg")'
                 await db.promiseQuery(sql)
             } catch (e) {
                 console.error(e)
-                return result.json({ resultat: "ko", missatge: "Error, funció llistatProductes: no s'ha pogut crear la taula productes"})  
+                return result.json({ resultat: "ko", missatge: "Error, funció llistatProductes: no s'ha pogut crear la taula productes"})
             }
         }
-    
+
         // Demana la informació de productes
         try {
             sql = 'SELECT * FROM productes'
             taula = await db.promiseQuery(sql)
         } catch (e) {
             console.error(e)
-            return result.json({ resultat: "ko", missatge: "Error, funció llistatProductes: ha fallat la crida a les dades"})  
-        }   
-    
+            return result.json({ resultat: "ko", missatge: "Error, funció llistatProductes: ha fallat la crida a les dades"})
+        }
+
         // Si hem aconseguit dades corectament, tornem la taula resultant
         if (typeof taula === 'object' && typeof taula.length === 'number') {
             result.json({ resultat: "ok", missatge: taula })
@@ -62,4 +64,3 @@ class Obj {
 
 // Export
 module.exports = Obj
-
